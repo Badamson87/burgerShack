aaa1a8aee2159c1dc44cb28210f26077888c3756
Brian Adamson <brianadamson87@gmail.com>
2019-01-14 16:14:28 -0700

    start menu

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using burgerShack.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace burgerShack.Controllers
+{
+  [Route("api/[controller")]
+  [ApiController]
+
+  public class MenuController : ControllerBase
+  {
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}
